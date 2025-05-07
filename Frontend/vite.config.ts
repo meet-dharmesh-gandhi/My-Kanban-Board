b873813aec79ebeaad42924ac494ed69ab66ce88
GeniusMind9999 <meetgandhi13062005@gmail.com>
2025-05-07 18:07:24 +0530

    added the vite-plugin-static-copy package to automate copying _redirects to dist at runtime

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +15,14 @@ export default defineConfig({
 				titleProp: true,
 			},
 			include: "**/*.svg",
+		}),
+		viteStaticCopy({
+			targets: [
+				{
+					src: "_redirects",
+					dest: ".",
+				},
+			],
 		}),
 	],
 });
